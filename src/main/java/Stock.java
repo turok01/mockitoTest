8e184153e4183d2dd96ca8d46c39c75afadc8dce
turok01 <44950560+turok01@users.noreply.github.com>
2020-01-17 11:41:48 +0300

    Portfolio class without mockito

---
+++ src/main/java/Stock.java
@@ -0,0 +1,26 @@
+public class Stock {
+    private String stockId;
+    private String name;
+    private int quantity;
+
+    public Stock(String stockId,String name, int quantity){
+        this.stockId = stockId;
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public void setStockId(String stockId) {
+        this.stockId = stockId;
+    }
+    public String getStockId(){
+        return stockId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getTicker() {
+        return name;
+    }
+}
